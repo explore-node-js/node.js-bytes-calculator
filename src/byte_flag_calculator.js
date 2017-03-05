6dcab2b86484bff78e260975e12fd8d2dad3a5ae
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-03-05 16:27:33 +0000

    init files + tests

---
+++ src/byte_flag_calculator.js
@@ -0,0 +1,15 @@
+module.exports = class ByteFlagCalculator {
+    static hasBytes(flags, flag) {
+        return (flags & flag) === flag;
+    }
+
+    static addBytes(flags, flag) {
+        return flags | flag;
+    }
+
+    static removeBytes(flags, flag) {
+        return ByteFlagCalculator.hasBytes(flags, flag)
+            ? flags & ~flag
+            : flags;
+    }
+};
